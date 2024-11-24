b9f54d836488c6e20e2d9d8b8baedf132a1194c7
rodgerswisdom <130440506+rodgerswisdom@users.noreply.github.com>
2024-11-24 23:43:43 +0300

    ....

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,23 +4,23 @@
  * ----------------------------------------------------------------------
  */
 
-document.addEventListener('contextmenu', function (e) {
-  e.preventDefault();
-});
+// document.addEventListener('contextmenu', function (e) {
+//   e.preventDefault();
+// });
 
-document.addEventListener('copy', function (e) {
-  e.preventDefault();
-});
+// document.addEventListener('copy', function (e) {
+//   e.preventDefault();
+// });
 
-document.addEventListener('keydown', function (e) {
-  // Disable Ctrl+U (view page source)
-  if (e.ctrlKey && e.keyCode === 85) {
-    e.preventDefault();
-  }
+// document.addEventListener('keydown', function (e) {
+//   // Disable Ctrl+U (view page source)
+//   if (e.ctrlKey && e.keyCode === 85) {
+//     e.preventDefault();
+//   }
 
-  // Disable certain key combinations that might open developer tools
-  if ((e.ctrlKey && e.shiftKey && e.keyCode === 73) ||  // Ctrl+Shift+I
-      (e.ctrlKey && e.shiftKey && e.keyCode === 74)) {  // Ctrl+Shift+J
-    e.preventDefault();
-  }
-});
+//   // Disable certain key combinations that might open developer tools
+//   if ((e.ctrlKey && e.shiftKey && e.keyCode === 73) ||  // Ctrl+Shift+I
+//       (e.ctrlKey && e.shiftKey && e.keyCode === 74)) {  // Ctrl+Shift+J
+//     e.preventDefault();
+//   }
+// });
